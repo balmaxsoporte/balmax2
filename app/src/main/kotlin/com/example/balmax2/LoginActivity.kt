ca69b801561c2cc9a0a06adcba55d1bbffd8b781
balmaxsoporte <balmaxsoporte@gmail.com>
2025-06-24 03:24:06 -0400

    Create LoginActivity.kt

---
+++ app/src/main/kotlin/com/example/balmax2/LoginActivity.kt
@@ -0,0 +1,32 @@
+package com.example.balmax2
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.balmax2.utils.ToastUtils
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val usernameEditText = findViewById<EditText>(R.id.usernameEditText)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (username == "admin" && password == "admin") {
+                startActivity(Intent(this, MainActivity::class.java))
+            } else {
+                ToastUtils.showToast(this, "Credenciales incorrectas")
+            }
+        }
+    }
+}
