b38668ed7f3b03dde69ba350d58d3b4c0f34f2cf
balmaxsoporte <balmaxsoporte@gmail.com>
2025-06-24 03:50:48 -0400

    Update FileUtils.kt

--- app/src/main/kotlin/com/example/balmax2/utils/FileUtils.kt
+++ app/src/main/kotlin/com/example/balmax2/utils/FileUtils.kt
@@ -2,12 +2,30 @@ package com.example.balmax2.utils
 
 import android.content.Context
 import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStreamWriter
 
 object FileUtils {
-    fun getOrCreateTxtFile(context: Context, filename: String): File {
-        val folder = context.getExternalFilesDir(null)
-        val file = File(folder, filename)
-        if (!file.exists()) file.createNewFile()
-        return file
+
+    fun exportArrendatariosToFile(context: Context, arrendatarios: List<String>) {
+        val file = File(context.filesDir, "arrendatarios.txt")
+        FileOutputStream(file).use { fos ->
+            OutputStreamWriter(fos).use { writer ->
+                for (plate in arrendatarios) {
+                    writer.write("\"$plate\"\n")
+                }
+            }
+        }
+    }
+
+    fun exportRegistroToFile(context: Context, registros: List<String>) {
+        val file = File(context.filesDir, "registro.txt")
+        FileOutputStream(file).use { fos ->
+            OutputStreamWriter(fos).use { writer ->
+                for (plate in registros) {
+                    writer.write("\"$plate\"\n")
+                }
+            }
+        }
     }
 }
