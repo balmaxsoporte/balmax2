eef3716fb7a850f29511732781163a39ac0ef4d6
balmaxsoporte <balmaxsoporte@gmail.com>
2025-07-04 08:21:21 -0400

    Delete app directory

--- app/src/main/kotlin/com/example/balmax2/data/Arrendatario.kt
+++
@@ -1,6 +0,0 @@
-package com.example.balmax2.data
-
-data class Arrendatario(
-    val id: Long = 0,
-    val plate: String
-)
--- app/src/main/kotlin/com/example/balmax2/data/ParkingSpot.kt
+++
@@ -1,8 +0,0 @@
-package com.example.balmax2.data
-
-data class ParkingSpot(
-    val number: Int,
-    val vehicleType: String,
-    val trailerType: String,
-    val plate: String
-)
